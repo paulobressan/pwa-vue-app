2aa66c5719560611ee17aefa2ef35f8fa6ba3c55
paulo.bressan <paulo.bressan@outlook.com>
2020-03-18 00:25:34 -0300

    feat(navbar): add a navbar with routes

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
 <template>
   <div>
-    <ul>
-      <li v-for="(link, index) in links" :key="index">
-        <router-link :to="link.to">{{ link.name }}</router-link>
-      </li>
-    </ul>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li v-for="(link, index) in links" :key="index" class="nav-item">
+            <router-link class="nav-link" :to="link.to">{{ link.name }}</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
     <main>
       <router-view />
     </main>
--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import "popper.js";
+import "jquery/dist/jquery.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
 Vue.config.productionTip = false
 
 new Vue({
